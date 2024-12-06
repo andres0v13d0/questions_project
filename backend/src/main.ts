f57442d0a8082dcd3ac50a2d35ac5ec07cd8093b
andres0v13d0 <oviedojonathan2001@gmail.com>
2024-12-05 23:10:03 -0500

    ssss

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Authorization',
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
